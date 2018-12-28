4f9ba99c2b9587f930f6d73e2d64ba644b20b259
erotic129@126.com <MVx43uH>
2018-12-28 17:41:56 +0800

    优化爬虫配置文件

--- scrapyuniversal/jsons.py
+++ scrapyuniversal/jsons.py
@@ -4,9 +4,6 @@ jsons = {
                 "website": "齐家网",
                 "type": "家装",
                 "table": "jia",
-                "settings": {
-                    "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"
-                },
                 "allowed_domains": ["jia.com"],
                 "start_urls": ["https://www.jia.com/xiamen/"],
                 "filter_urls": ["tuku", "luntan", "javascript"]
@@ -16,9 +13,6 @@ jsons = {
                 "website": "安居客",
                 "type": "租房",
                 "table": "anjuke",
-                "settings": {
-                    "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"
-                },
                 "allowed_domains": ["anjuke.com"],
                 "start_urls": ["https://www.anjuke.com/fz/cm/"],
                 "filter_urls": [],
@@ -30,9 +24,6 @@ jsons = {
                 "website": "我图网",
                 "type": "素材",
                 "table": "ooopic",
-                "settings": {
-                    "USER_AGENT": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"
-                },
                 "allowed_domains": ["ooopic.com"],
                 "start_urls": ["http://www.ooopic.com/shineisucai/", "http://www.ooopic.com/zhuangshihua/"]
             }
